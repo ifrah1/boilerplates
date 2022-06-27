d13450ff23612033a71762ea2ea1719a66bf8252
ifrah1 <arifrah125@gmail.com>
2022-06-26 21:10:35 -0400

    added client code to connect to sever

--- client-server-boilerplate/client/src/App.js
+++ client-server-boilerplate/client/src/App.js
@@ -1,6 +1,23 @@
 import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from 'react';
+
 
 function App() {
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    /* NOTE for connecting server and client if both are hosted in same sever
+     * in package.json for client side we add a key value of "proxy", this 
+     * allows for react app to check the server folder and go to its route
+    */
+    axios.get("/api/test/data").then(response => {
+      console.log(response.data);
+    });
+
+  }, []);
+
+
   return (
     <div className="App">
       HELLO WORLD
